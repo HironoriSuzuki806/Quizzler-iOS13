3962aaa1a466761afaddf90d62e2759fbd66e8f3
鈴木 宏典 <hironori.suzuki@frog-pod.com>
2020-07-10 19:22:11 +0900

    ViewControllerに全ての機能を実装した。

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,22 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by 鈴木宏典 on 2020/07/10.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+    
+    
+    
+}
